79ec8765d7ffccd180ada49ee4635b31fc414d28
Vinka Prpić <maggie222222@gmail.com>
2017-10-22 13:57:30 +0200

    Connected to SQLite database.

---
+++ Assets/Scripts/DBManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.Data;
+using Mono.Data.Sqlite;
+
+public class DBManager : MonoBehaviour {
+
+    private string connectionString;
+
+	// Use this for initialization
+	void Start () {
+        connectionString = "URI=file:" + Application.dataPath + "/DB/DB.db";
+        GetItems();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void GetItems()
+    {
+        //when using ends it calls .dispose() which disposes of the connection
+        using (IDbConnection dbConnection = new SqliteConnection(connectionString))
+        {
+            dbConnection.Open();
+            using(IDbCommand dbCmd = dbConnection.CreateCommand())
+            {
+                string sqlQuery = "SELECT * FROM playerInventory";
+                dbCmd.CommandText = sqlQuery;
+
+                using (IDataReader reader = dbCmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        Debug.Log(reader.GetString(1) + " " + reader.GetInt32(2));
+                    }
+
+                    dbConnection.Close();
+                    reader.Close();
+                }
+            }
+        }
+    }
+}
